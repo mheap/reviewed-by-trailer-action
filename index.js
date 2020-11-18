397893ca4d814385603510a2a5fed4923375423a
Michael Heap <m@michaelheap.com>
2020-11-18 10:53:48 +0000

    Action working

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ const { Toolkit } = require("actions-toolkit");
 // Run your GitHub Action!
 Toolkit.run(
   async (tools) => {
+    let body = tools.context.payload.pull_request.body;
+
+    // Fetch the user
+    const username = tools.context.payload.review.user.login;
+    const { data: user } = await tools.github.users.getByUsername({
+      username,
+    });
+
+    // Make sure there is an additional blank line if there is no reviewed by trailer
+    if (!body.includes("Reviewed-by")) {
+      body += "\n";
+    }
+
     // Append if it passed
+    body += `\nReviewed-by: ${user.name} &lt;${user.email}>`;
+
+    tools.github.issues.update({
+      ...tools.context.issue,
+      body,
+    });
 
     // Append if it failed + feature is enabled
+    // @TODO
 
     // Append if neutral + feature is enabled
+    // @TODO
     tools.exit.success("We did it!");
   },
   {
--- index.test.js
+++ index.test.js
@@ -1,23 +1,59 @@
-const { Toolkit } = require('actions-toolkit')
+const { Toolkit } = require("actions-toolkit");
 
-describe('Add Reviewed-by trailer', () => {
-  let action, tools
+describe("Add Reviewed-by trailer", () => {
+  let action, tools;
 
   // Mock Toolkit.run to define `action` so we can call it
-  Toolkit.run = jest.fn((actionFn) => { action = actionFn })
+  Toolkit.run = jest.fn((actionFn) => {
+    action = actionFn;
+  });
   // Load up our entrypoint file
-  require('.')
+  require(".");
 
   beforeEach(() => {
-    // Create a new Toolkit instance
-    tools = new Toolkit()
-    // Mock methods on it!
-    tools.exit.success = jest.fn()
-  })
+    jest.resetModules();
 
-  it('exits successfully', () => {
-    action(tools)
-    expect(tools.exit.success).toHaveBeenCalled()
-    expect(tools.exit.success).toHaveBeenCalledWith('We did it!')
-  })
-})
+    tools = mockEvent("pull_request_review", {
+      action: "submitted",
+      pull_request: {
+        body: "This is an example body",
+        number: 27,
+      },
+      review: {
+        user: {
+          login: "mheap",
+        },
+      },
+      repository: {
+        owner: {
+          login: "mheap",
+        },
+        name: "action-test",
+      },
+    });
+  });
+
+  it("works and makes API calls as a run harness", async () => {
+    tools.exit.success = jest.fn();
+    await action(tools);
+    expect(tools.exit.success).toHaveBeenCalled();
+    expect(tools.exit.success).toHaveBeenCalledWith("We did it!");
+  });
+});
+
+function mockEvent(name, mockPayload) {
+  jest.mock(
+    "/github/workspace/event.json",
+    () => {
+      return mockPayload;
+    },
+    {
+      virtual: true,
+    }
+  );
+
+  process.env.GITHUB_EVENT_NAME = name;
+  process.env.GITHUB_EVENT_PATH = "/github/workspace/event.json";
+
+  return new Toolkit();
+}
